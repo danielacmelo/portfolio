ef0455436314d4c659864ce090ee92fb1fab695d
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-07-10 18:44:33 -0700

    Update work archive

--- archive-dcm-work.php
+++ archive-dcm-work.php
@@ -1,51 +1,72 @@
 <?php
 /**
- * The template for displaying archive pages
+ * The template for displaying work archive
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package DCM_theme
  */
 
-get_header();
-?>
+ get_header();
+ ?>
+ 
+     <main id="primary" class="site-main">
+ 
+         <?php if ( have_posts() ) : ?>
+ 
+             <header class="page-header">
+                 <?php
+                 the_archive_description( '<div class="archive-description">', '</div>' );
+                 ?>
+             </header><!-- .page-header -->
+ 
+             <?php
+ 
+             $args = array(
+                 'post_type'      => 'dcm-work',
+                 'posts_per_page' => -1,
+                 'orderby'        => 'title',
+                 'order'          => 'ASC'
+             );
+             
+             $query = new WP_Query( $args );
+ 
+             if ( $query->have_posts() ) {
+                 echo '<section class="works">';
+                 while( $query->have_posts() ) {
+                     $query->the_post(); 
+                    // Get the ACF field value
+                    $tools_and_skills = get_field('tools_and_skills');
+                    // Check if $tools_and_skills is an array and convert it to a string
+                    if (is_array($tools_and_skills)) {
+                    $tools_and_skills = implode(' - ', $tools_and_skills);
+                    }
+                    ?>
+                    <article class="works">
+                                <a href="<?php the_permalink(); ?>">
+                                    <h2><?php the_title(); ?></h2>
+                                    <?php the_post_thumbnail('landscape-projects'); ?>
+                                </a>
+                                <h3><?php echo esc_html($tools_and_skills); ?></h3>
+                            </article>
+                            <?php
+                        }
+                        wp_reset_postdata();
+                        echo '</section>';
+                    } 
 
-	<main id="primary" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+                    ?>
+                       
+         <?php     
+         else :
+ 
+             get_template_part( 'template-parts/content', 'none' );
+ 
+         endif;
+         ?>
+ 
+     </main><!-- #main -->
+ 
+ <?php
+ get_footer();
+ 
