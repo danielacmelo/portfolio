c185c76c1d8c42537cd632764c8d488161caf6d6
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-07-12 21:11:56 -0700

    Update single work

--- single-dcm-work.php
+++ single-dcm-work.php
@@ -27,20 +27,8 @@ get_header();
                             <?php the_post_thumbnail('landscape_projects'); ?>
                             <?php the_content(); ?>
                     </article>    
-
-                <?php     
-                the_post_navigation(
-				    array(
-					    'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous Work:', 'dcm_theme' ) . '</span> <span class="nav-title">%title</span>',
-					    'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next Work:', 'dcm_theme' ) . '</span> <span class="nav-title">%title</span>',
-				    )
-			    );
-                ?>
-
                
-                </div><!-- .entry-content -->       
-                
-                
+                </div><!-- .entry-content -->        
             
         <?php 
         endwhile;
